733116cc1d7ccaac66b4c4412f996bea930a08e6
Grohot9 <igor.samarsky8@gmail.com>
2022-11-21 11:05:43 +0300

    Deleted for statements

--- homework_1/decorators/cache_decorator.py
+++ homework_1/decorators/cache_decorator.py
@@ -7,28 +7,26 @@ def lfu_cache(max_limit=64):
     def internal(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            new_cache_key = (args, tuple(kwargs.values()))
-            for cache_key, cache_value in wrapper._cache.items():
-                if new_cache_key == cache_key:
-                    cache_data = cache_value[0]
-                    count_of_calls = cache_value[1]
-                    # changing the number of calls
-                    cache_value = [cache_data, count_of_calls + 1]
-                    wrapper._cache.update({cache_key: cache_value})
-                    return cache_data
+            cache_key = (args, tuple(kwargs.values()))
+            if cache_key in wrapper._cache_keys:
+                # changing the count of calls
+                wrapper._counts_of_calls[wrapper._cache_keys.index(cache_key)] += 1
+                return wrapper._cache[cache_key]
             cache_data = func(*args, **kwargs)
             if len(wrapper._cache) >= max_limit:
-                for cache_key, cache_value in wrapper._cache.items():
-                    # deleting data with the least number of calls
-                    if cache_value[1] == wrapper.minimal_count_of_calls:
-                        wrapper._cache.pop(cache_key)
-                        break
+                index_for_del = wrapper._counts_of_calls.index(min(wrapper._counts_of_calls))
+                # deleting data with the least count of calls
+                wrapper._cache.pop(wrapper._cache_keys[index_for_del])
+                wrapper._cache_keys.pop(index_for_del)
+                wrapper._counts_of_calls.pop(index_for_del)
             # adding new data in the cache
-            wrapper._cache.update({new_cache_key: [cache_data, 1]})
-            wrapper.minimal_count_of_calls = 1
+            wrapper._cache.update({cache_key: cache_data})
+            wrapper._cache_keys.append(cache_key)
+            wrapper._counts_of_calls.append(1)
 
-        wrapper.minimal_count_of_calls = 1
         wrapper._cache = dict()
+        wrapper._cache_keys = list()
+        wrapper._counts_of_calls = list()
         return wrapper
 
     return internal
