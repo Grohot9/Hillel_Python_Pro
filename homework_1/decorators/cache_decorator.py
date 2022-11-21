728f8a78d71118afce3b0dcec15be94b66c1fee2
Grohot9 <igor.samarsky8@gmail.com>
2022-11-21 07:59:17 +0300

    Added comments in English. Removed "return cache_data"

--- homework_1/decorators/cache_decorator.py
+++ homework_1/decorators/cache_decorator.py
@@ -8,40 +8,24 @@ def lfu_cache(max_limit=64):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             new_cache_key = (args, tuple(kwargs.values()))
-            # Структура кэша = {cache_key: {cache_data: count_of_calls}, ...} до max_limit
-
-            # Проверяем наличие полученного ключа в кэше
             for cache_key, cache_value in wrapper._cache.items():
-                # Если находим ключ - изменяем колличество вызовов и возвращаем данные
                 if new_cache_key == cache_key:
-                    # Достаём данные из вложенного словаря - {cache_data: count_of_calls}
-                    cache_data = tuple(cache_value.keys())[0]
-                    count_of_calls = tuple(cache_value.values())[0]
-                    # Изменяем колличество вызовов
-                    cache_value = {cache_data: count_of_calls + 1}
-                    # Новый кэш = {cache_key: {cache_data: count_of_calls + 1}, ...} до max_limit
+                    cache_data = cache_value[0]
+                    count_of_calls = cache_value[1]
+                    # changing the number of calls
+                    cache_value = [cache_data, count_of_calls + 1]
                     wrapper._cache.update({cache_key: cache_value})
-                    # Возвращаем данные
                     return cache_data
-
-            # Если полученного ключа в кэше нет - вызываем функцию и записываем в кэш
             cache_data = func(*args, **kwargs)
-
-            # Проверяем наличие места в кэше, если места нету - очищаем данные с наименьшим колличеством вызовов
             if len(wrapper._cache) >= max_limit:
                 for cache_key, cache_value in wrapper._cache.items():
-                    # Очищаем данные с наименьшим колличеством вызовов
-                    if tuple(cache_value.values()) == wrapper.minimal_count_of_calls:
+                    # deleting data with the least number of calls
+                    if cache_value[1] == wrapper.minimal_count_of_calls:
                         wrapper._cache.pop(cache_key)
                         break
-
-            # Записываем в кэш полученные данные
-            wrapper._cache.update({new_cache_key: {cache_data: 1}})
-
-            # Обновляем наименьшое колличество вызовов на актуальное
+            # adding new data in the cache
+            wrapper._cache.update({new_cache_key: [cache_data, 1]})
             wrapper.minimal_count_of_calls = 1
-
-            return cache_data
 
         wrapper.minimal_count_of_calls = 1
         wrapper._cache = dict()
